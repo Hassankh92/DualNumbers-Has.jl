605ab3376e58a8016dfb87e2c33089d25b0bafa3
Hassan <hassan.kh92@gmail.com>
2023-01-16 15:08:17 -0500

    added the conjugate, inverse and division

--- src/.ipynb_checkpoints/DualNumbers-checkpoint.jl
+++ src/.ipynb_checkpoints/DualNumbers-checkpoint.jl
@@ -24,6 +24,34 @@ Base.:*(a::Number, x::Dual) = Dual(a*x.real, a*x.dual)
 Base.:*(x::Dual, a::Number) = Dual(x.real*a , x.dual*a)
 
 
+#  Multiplications
+#  x = a + epsilon *b
+#  y = c + epsilon *c
+#  epsilon^2 = 0
+#   x * y = a*c + epsilon * (a*d + b*c)
+
+Base.:*{x::Dual, y::Dual} = Dual(x.real * y.real , x.real * y.dual + x.dual * y.real)
+
+#  Conjugate
+Base.conj(x::Dual) = Dual(x.real , -x.dual)
+
+
+# Inverse 
+#      1 / (a + epsilon * b)
+#   = (a - eps b ) / (a + eps b)(a - eps b)
+#   = (a - eps b)  / (a^2) 
+#   = (a - eps b)  * (1/a^2) 
+
+Base.inv(x::Dual) = conj(x) * 1/(x.real^2)
+
+# Division
+
+Base.:/(x::Dual,y::Dual) = x * inv(y)
+
+
+
+
+
 
 
 end # module DualNumbers
