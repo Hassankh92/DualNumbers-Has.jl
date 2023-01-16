0e24f15261a884d1fccf338e7399dd8555f296cb
Hassan <hassan.kh92@gmail.com>
2023-01-16 14:47:39 -0500

    added more dual algebra operations to the package, including scalar multiplication, and negative

--- src/.ipynb_checkpoints/DualNumbers-checkpoint.jl
+++ src/.ipynb_checkpoints/DualNumbers-checkpoint.jl
@@ -10,8 +10,20 @@ struct Dual{T}
 	dual::T
 end
 
+Base.:-(x::Dual) = Dual(-x.real, -x.dual)
+Base.:+(x::Dual) = Dual(+x.real, +x.dual)
+
 Base.:+(x::Dual, y::Dual) = Dual(x.real + y.real, x.dual + y.dual)
-Base.:-(x::Dual, y::Dual) = Dual(x.real - y.real, x.dual - y.dual)
+# Base.:-(x::Dual, y::Dual) = Dual(x.real - y.real, x.dual - y.dual)
+Base.:-(x::Dual, y::Dual) = x + -y
+
+
+#This part is for scalar multiplication of dual numbers
+
+Base.:*(a::Number, x::Dual) = Dual(a*x.real, a*x.dual)
+Base.:*(x::Dual, a::Number) = Dual(x.real*a , x.dual*a)
+
+
 
 
 end # module DualNumbers
